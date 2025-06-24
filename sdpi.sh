0d2f04666cad2bbe00505b148ea14b56f76443af
xaycit <lzsetttings01@gmail.com>
2025-06-24 20:11:42 +0700

    Create sdpi.sh

---
+++ sdpi.sh
@@ -0,0 +1,32 @@
+#!/system/bin/sh
+
+# Packages to monitor
+PKG1="com.dts.freefireth"
+PKG2="com.dts.freefiremax"
+
+# Track current state
+STATE="stopped"
+
+while true; do
+    # Check if either app is running
+    pid1=$(pidof "$PKG1")
+    pid2=$(pidof "$PKG2")
+
+    if [ -n "$pid1" ] || [ -n "$pid2" ]; then
+        if [ "$STATE" = "stopped" ]; then
+            cmd notification post -S bigtext -t 'Smart Dpi By Lanzsettings' 'Tag' 'Active' > /dev/null 2>&1
+            
+            STATE="running"
+        fi
+    else
+        if [ "$STATE" = "running" ]; then
+            cmd notification post -S bigtext -t 'Smart Dpi By Lanzsettings' 'Tag' 'Not Active' > /dev/null 2>&1
+            
+            STATE="stopped"
+        fi
+    fi
+
+    sleep 2
+done
+
+exit 0
