bee633ff1c6b5d296a4f703d9c140e650f4bde29
xaycit <lzsetttings01@gmail.com>
2025-06-25 21:52:54 +0700

    Update delexsdpi.sh

--- delexsdpi.sh
+++ delexsdpi.sh
@@ -1,5 +1,9 @@
+echo "[*] Deactivating Smart DPI V1..."
+
 rm -f /data/local/tmp/sdpi.sh
 rm -f /tmp/sdpi.sh
 rm -f /storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Temp/android/sdpi.sh
 rm -f /storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Temp/android/sdpi.sh
 pkill -f sdpi.sh
+
+echo "[✓] Smart DPI V1 Successfully Deactivated"
