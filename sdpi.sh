7730739ec03861bc3dd4f3169b1fca4b7e8fc3b8
xaycit <lzsetttings01@gmail.com>
2025-06-26 14:06:21 +0700

    Update sdpi.sh

--- sdpi.sh
+++ sdpi.sh
@@ -15,13 +15,13 @@ while true; do
     if [ -n "$pid1" ] || [ -n "$pid2" ]; then
         if [ "$STATE" = "stopped" ]; then
             cmd notification post -S bigtext -t 'Smart Dpi By Lanzsettings' 'Tag' 'Active' > /dev/null 2>&1
-            
+            wm density 265
             STATE="running"
         fi
     else
         if [ "$STATE" = "running" ]; then
             cmd notification post -S bigtext -t 'Smart Dpi By Lanzsettings' 'Tag' 'Not Active' > /dev/null 2>&1
-            
+            wm density reset
             STATE="stopped"
         fi
     fi
