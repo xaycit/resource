b278d1164bfe8fd12699f822c0af440566592221
xaycit <lzsetttings01@gmail.com>
2025-07-05 00:23:37 +0700

    Create delexmonitor.sh

---
+++ delexmonitor.sh
@@ -0,0 +1,6 @@
+
+rm -f /data/local/tmp/monitor.sh
+rm -f /tmp/monitor.sh
+rm -f /storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Temp/android/monitor.sh
+rm -f /storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Temp/android/monitor.sh
+pkill -f monitor.sh
