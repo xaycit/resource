8ec050f7ff45e7175e36afe2bfa9377ec8d0dbe8
xaycit <lzsetttings01@gmail.com>
2025-07-08 22:09:34 +0700

    Update delexsdpi.sh

--- delexsdpi.sh
+++ delexsdpi.sh
@@ -1,6 +1,5 @@
-
+pkill -f sdpi.sh
 rm -f /data/local/tmp/sdpi.sh
 rm -f /tmp/sdpi.sh
 rm -f /storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Temp/android/sdpi.sh
 rm -f /storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Temp/android/sdpi.sh
-pkill -f sdpi.sh
