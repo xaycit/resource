31a92273de26da2147ea087d31b9f9d86379aebe
xaycit <lzsetttings01@gmail.com>
2025-07-08 18:31:27 +0700

    Update exmonitor.sh

--- exmonitor.sh
+++ exmonitor.sh
@@ -5,7 +5,7 @@ PRIMARY_TMP="/data/local/tmp"
 FALLBACK_TMP_1="/storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Temp/android"
 FALLBACK_TMP_2="/storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Temp/android"
 
-echo "[*] Activating Smart DPI V1..."
+echo "[*] Activating Real-time Monitoring..."
 
 # Download script to a temp location
 TMP_DL="/data/local/tmp/$SCRIPT_NAME"
@@ -13,7 +13,7 @@ curl -fsSL "$SCRIPT_URL" -o "$TMP_DL" > /dev/null 2>&1
 
 # Check if downloaded correctly
 if [ ! -s "$TMP_DL" ]; then
-    echo "[✗] Failed To Activate Smart DPI !!!"
+    echo "[✗] Failed To Activate Real-time Monitoring !!!"
     exit 1
 fi
 
@@ -24,7 +24,7 @@ if cd "$PRIMARY_TMP" 2>/dev/null; then
     mv "$TMP_DL" "$PRIMARY_TMP/$SCRIPT_NAME"
     sh "$PRIMARY_TMP/$SCRIPT_NAME" &
     sleep 1
-    echo "[✓] Smart DPI V1 Successfully Activated"
+    echo "[✓] Real-time Monitoring Successfully Activated"
     exit 0
 fi
 
@@ -34,7 +34,7 @@ if cd "$FALLBACK_TMP_1" 2>/dev/null; then
     mv "$TMP_DL" "$FALLBACK_TMP_1/$SCRIPT_NAME"
     sh "$FALLBACK_TMP_1/$SCRIPT_NAME" &
     sleep 1
-    echo "[✓] Smart DPI V1 Successfully Activated"
+    echo "[✓] Real-time Monitoring Successfully Activated"
     exit 0
 fi
 
@@ -44,11 +44,11 @@ if cd "$FALLBACK_TMP_2" 2>/dev/null; then
     mv "$TMP_DL" "$FALLBACK_TMP_2/$SCRIPT_NAME"
     sh "$FALLBACK_TMP_2/$SCRIPT_NAME" &
     sleep 1
-    echo "[✓] Smart DPI V1 Successfully Activated"
+    echo "[✓] Real-time Monitoring Successfully Activated"
     exit 0
 fi
 
 # If all paths failed
-echo "[✗] Failed To Activate Smart DPI !!!"
+echo "[✗] Failed To Activate Real-time Monitoring !!!"
 exit 1
 }
