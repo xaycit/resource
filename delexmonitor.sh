9a5f2919b36f1f3f2c0eea467fe4aaa0283717c1
xaycit <lzsetttings01@gmail.com>
2025-07-08 22:09:03 +0700

    Update delexmonitor.sh

--- delexmonitor.sh
+++ delexmonitor.sh
@@ -1,6 +1,5 @@
-
+pkill -f monitor.sh
 rm -f /data/local/tmp/monitor.sh
 rm -f /tmp/monitor.sh
 rm -f /storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Temp/android/monitor.sh
 rm -f /storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Temp/android/monitor.sh
-pkill -f monitor.sh
