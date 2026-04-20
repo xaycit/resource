fbe3d8c0265ddf0cbbc49ce1e3b31c3ec54132de
xaycit <lzsetttings01@gmail.com>
2026-04-20 12:58:58 +0700

    Update delaiming.sh

--- delaiming.sh
+++ delaiming.sh
@@ -14,8 +14,31 @@ fetch() {
 }
 
 exechz() {
-    fetch "$hz"
+    setprop debug.sf.use_phase_offsets_as_durations ""
+    setprop debug.sf.late.sf.duration ""
+    setprop debug.sf.late.app.duration ""
+    setprop debug.sf.early.sf.duration ""
+    setprop debug.sf.early.app.duration ""
+    setprop debug.sf.earlyGl.sf.duration ""
+    setprop debug.sf.earlyGl.app.duration ""
+
+    setprop debug.sf.disable_client_composition_cache ""
+    setprop debug.sf.early_phase_offset_ns ""
+    setprop debug.sf.early_gl_phase_offset_ns ""
+    setprop debug.sf.high_fps_late_app_phase_offset_ns ""
+    setprop debug.sf.high_fps_late_sf_phase_offset_ns ""
+    setprop debug.sf.high_fps_early_phase_offset_ns ""
+    setprop debug.sf.high_fps_early_gl_phase_offset_ns ""
+
+    settings delete global game_driver_all_apps
+    settings delete global game_driver_opt_out_apps
+    settings delete global game_driver_opt_in_apps
+
+    settings delete global updatable_driver_all_apps
+    settings delete global updatable_driver_production_opt_out_apps
+    settings delete global updatable_driver_production_opt_in_apps
 }
+
 
 main() {
     cmd game reset com.dts.freefireth
@@ -37,10 +60,15 @@ main() {
 
     settings put system pointer_speed 0
     settings put system pointer_acceleration 0
+    settings put system minimum_pointer_speed 0 
+    settings put system maximum_pointer_speed 7
 
     cmd power set-adaptive-power-saver-enabled true
     cmd power set-fixed-performance-mode-enabled false
     cmd power set-mode 1
+    cmd settings put secure screensaver_activate_on_dock 1
+    cmd settings put secure screensaver_activate_on_sleep 1
+    cmd settings put secure screensaver_enabled 1
 
     cmd thermalservice override-status 1
 
@@ -48,6 +76,8 @@ main() {
     cmd compile -m everything --reset com.dts.freefiremax
 
     settings put secure touch_blocking_period 500
+    settings put secure touch_block_delay 100 
+    settings put system touch_sensitivity 3 
 
     settings put system glove_mode 0
     settings put system screen_glove_mode_enabled 0
@@ -65,8 +95,15 @@ main() {
     settings delete system touch.input_flinger.calibration
 
     cmd device_config delete input_native_boot palm_rejection_enabled
+    
+    settings put global windowsmgr.max_events_per_sec 60
+    settings put system windowsmgr.max_events_per_sec 60 
 
     pkill -f dpi
+    pkill -f sc
+    pkill -f compiler
+    pkill -f lib
+    pkill -f "/data/local/tmp/lib" || pkill -f "/storage/emulated/0/Android/data/me.piebridge.brevent/lib"
 }
 
 main >/dev/null 2>&1
